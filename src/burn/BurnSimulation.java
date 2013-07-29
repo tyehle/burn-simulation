6b9caf828bed0b6b6f52dcb936fd4b4f92e7d556
Tobin <Tobin@Loki>
2013-07-28 23:33:03 -0600

    a long time ago, in a galaxy far far away ...

---
+++ src/burn/BurnSimulation.java
@@ -0,0 +1,17 @@
+package burn;
+
+/**
+ *
+ * @author Tobin
+ */
+public class BurnSimulation
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world");
+    }
+}
