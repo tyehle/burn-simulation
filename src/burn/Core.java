310aadf1603a85e092a6df6acd7d3e19ac76b574
Tobin <Tobin@Loki>
2013-07-29 09:08:37 -0600

    some core logic done

--- src/burn/Core.java
+++ src/burn/Core.java
@@ -13,8 +13,28 @@ public class Core
         this.diameter = diameter;
     }
     
+    /**
+     * Gets the smallest distance between the center of the core and the given
+     * position.
+     * @param p The position to calculate the distance from
+     * @return The distance from the core to the given position
+     */
     public double getDistance(Position p)
     {
         return Math.sqrt(p.getX()*p.getX() + p.getY()*p.getY());
+    }
+    
+    /**
+     * Gets the time it would take for the core to ignite the given bubble in
+     * the presence of no other bubbles.
+     * @param b The bubble to calculate the ignition time of
+     * @param burnRate The burn rate of the grain
+     * @return The time from the ignition of the core at which the bubble will
+     * ignite
+     */
+    public double getIgnitionTime(Bubble b, double burnRate)
+    {
+        double d = getDistance(b.getPosition()) - diameter - b.getRadius();
+        return d / burnRate;
     }
 }
